2d82654ace008e447b65cf64273523e6a047ca41
LegitKillerOP <contact.legitkiller@gmail.com>
2025-06-27 13:13:13 +0530

    Basic webhooks

--- src/main/java/me/legit/bungeefunmod/Bungeefunmod.java
+++ src/main/java/me/legit/bungeefunmod/Bungeefunmod.java
@@ -2,6 +2,7 @@ package me.legit.bungeefunmod;
 
 import me.legit.bungeefunmod.commands.mod.*;
 import me.legit.bungeefunmod.commands.fun.*;
+import me.legit.bungeefunmod.discord.WebhookLogger;
 import me.legit.bungeefunmod.managers.ConfigManager;
 import net.md_5.bungee.api.plugin.Plugin;
 
@@ -9,12 +10,14 @@ public final class Bungeefunmod extends Plugin {
 
     private static Bungeefunmod instance;
     private ConfigManager configManager;
+    private WebhookLogger webhookLogger;
 
     @Override
     public void onEnable() {
         instance = this;
 
         configManager = new ConfigManager(this);
+        webhookLogger = new WebhookLogger();
 
         getProxy().getPluginManager().registerCommand(this, new ModReloadCommand());
         getProxy().getPluginManager().registerCommand(this, new HugCommand());
@@ -24,11 +27,23 @@ public final class Bungeefunmod extends Plugin {
         getProxy().getPluginManager().registerCommand(this, new EightBallCommand());
 
         getLogger().info("BungeeFunModeration enabled.");
+
+        webhookLogger.sendEmbed(
+                "Plugin Enabled",
+                "BungeeFunModeration plugin has started successfully.",
+                3066993
+        );
     }
 
     @Override
     public void onDisable() {
         getLogger().info("BungeeFunModeration disabled.");
+
+        webhookLogger.sendEmbed(
+                "Plugin Disabled",
+                "BungeeFunModeration plugin has been stopped.",
+                15158332
+        );
     }
 
     public static Bungeefunmod getInstance() {
@@ -38,4 +53,8 @@ public final class Bungeefunmod extends Plugin {
     public ConfigManager getConfigManager() {
         return configManager;
     }
+
+    public WebhookLogger getWebhookLogger() {
+        return webhookLogger;
+}
 }
--- src/main/java/me/legit/bungeefunmod/discord/WebhookLogger.java
+++ src/main/java/me/legit/bungeefunmod/discord/WebhookLogger.java
@@ -1,4 +1,67 @@
 package me.legit.bungeefunmod.discord;
 
+import me.legit.bungeefunmod.Bungeefunmod;
+
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.logging.Level;
+
 public class WebhookLogger {
+
+    private final String webhookUrl;
+
+    public WebhookLogger() {
+        this.webhookUrl = Bungeefunmod.getInstance().getConfigManager()
+                .getConfig().getString("webhook-url", "");
+    }
+
+    /**
+     * Sends a simple embed message to Discord webhook.
+     * @param title The embed title.
+     * @param description The embed description.
+     * @param color The embed color integer (decimal).
+     */
+    public void sendEmbed(String title, String description, int color) {
+        if (webhookUrl.isEmpty()) {
+            Bungeefunmod.getInstance().getLogger().warning("Discord webhook URL is not configured.");
+            return;
+        }
+
+        try {
+            URL url = new URL(webhookUrl);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("POST");
+            con.setDoOutput(true);
+            con.setRequestProperty("Content-Type", "application/json");
+
+            String jsonPayload = "{"
+                    + "\"embeds\":[{"
+                    + "\"title\":\"" + escapeJson(title) + "\","
+                    + "\"description\":\"" + escapeJson(description) + "\","
+                    + "\"color\":" + color
+                    + "}]"
+                    + "}";
+
+            try (OutputStream os = con.getOutputStream()) {
+                byte[] input = jsonPayload.getBytes(StandardCharsets.UTF_8);
+                os.write(input, 0, input.length);
+            }
+
+            int responseCode = con.getResponseCode();
+            if (responseCode != 204 && responseCode != 200) {
+                Bungeefunmod.getInstance().getLogger()
+                        .warning("Failed to send webhook message. Response code: " + responseCode);
+            }
+
+            con.disconnect();
+        } catch (Exception e) {
+            Bungeefunmod.getInstance().getLogger().log(Level.SEVERE, "Error sending Discord webhook message", e);
+        }
+    }
+
+    private String escapeJson(String text) {
+        return text.replace("\"", "\\\"").replace("\n", "\\n");
+    }
 }
